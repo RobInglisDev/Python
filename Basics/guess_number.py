29000cba46669e476d8c0bfd56bb387aa1166840
Rob Inglis (Group) <Rob.Inglis@dimensiondata.com>
2019-01-07 13:29:01 +0200

    finished app

--- Basics/guess_number.py
+++ Basics/guess_number.py
@@ -6,13 +6,14 @@ print('---------------------------------------------')
 
 mynum = random.randint(0, 100)
 guess = -1
+name = input('Please enter your name -> ')
 
 while guess != mynum:
-    guess_txt = input('Guess the number that I am thinking of....(' + str(mynum)+')  ')
+    guess_txt = input('Guess the number that I am thinking of....({0}) -> '.format(mynum))
     guess = int(guess_txt)
     if guess < mynum:
-        print('Too Low')
+        print('Sorry {}, your guess of {} was Too Low'.format(name,guess))
     elif guess > mynum:
-        print('Too High')
+        print('Sorry {}, your guess of {} was Too High'.format(name,guess))
     else:
-        print('WELL DONE, YOU WIN!')
+        print('WELL DONE {}, your guess of {} was Correct!'.format(name,guess))
